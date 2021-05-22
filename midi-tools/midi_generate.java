4789f63d83af8579f617794c59fa61183a416bd6
Kit Maier <8maier@gmail.com>
2021-05-22 00:12:52 -0700

    added functionality to control instrument selection from composition files

--- midi-tools/midi_generate.java
+++ midi-tools/midi_generate.java
@@ -12,11 +12,10 @@
 import java.io.*;
 import java.util.*;
 import javax.sound.midi.*;
-public class midi_generate
-{
+public class midi_generate {
 	public static void main(String argv[]) throws Exception {
 		String[] fileList = {
-			"composition_20210519_3"
+			"composition_20210521_3"
 		};
 		for(String fileName : fileList) {
 			System.out.println(fileName);
@@ -39,20 +38,9 @@ public class midi_generate
 		setupTrack(V);
 		maxEnd = 0;
 
-		//setInstrument(L,0,PIANO__ACOUSTIC_GRAND_PIANO);
-		setInstrument(L,0,STRINGS__ORCHESTRAL_HARP);
-		//setInstrument(L,0,GUITAR__ACOUSTIC_GUITAR_NYLON);
-		//setInstrument(L,0,REED__ENGLISH_HORN);
-		//setInstrument(L,0,STRINGS__CELLO);
-		//setInstrument(L,0,REED__SOPRANO_SAX);
-		//setInstrument(R,1,PIANO__ACOUSTIC_GRAND_PIANO);
-		setInstrument(R,1,STRINGS__ORCHESTRAL_HARP);
-		//setInstrument(R,1,GUITAR__ACOUSTIC_GUITAR_NYLON);
-		//setInstrument(R,1,REED__ENGLISH_HORN);
-		//setInstrument(R,1,REED__SOPRANO_SAX);
-		//setInstrument(R,1,STRINGS__VIOLA);
-		setInstrument(V,2,REED__ENGLISH_HORN);
-		//setInstrument(V,2,STRINGS__VIOLA);
+		setInstrument(L,0,getInstrument("PIANO__ACOUSTIC_GRAND_PIANO"));
+		setInstrument(R,1,getInstrument("PIANO__ACOUSTIC_GRAND_PIANO"));
+		setInstrument(V,2,getInstrument("PIANO__ACOUSTIC_GRAND_PIANO"));
 		setInstrument(P,9,00); // percussion
 
 		Scanner scan = new Scanner(new File(fileName+".txt"));
@@ -66,6 +54,18 @@ public class midi_generate
 				}
 				if(line.contains("tempo:")&&line.contains("fast")) {
 					ticksPerBeat = 8;
+				}
+				if(line.contains("L_instrument")) {
+					String instrumentName = line.replaceAll(".*[: ]","");
+					setInstrument(L,0,getInstrument(instrumentName));
+				}
+				if(line.contains("R_instrument")) {
+					String instrumentName = line.replaceAll(".*[: ]","");
+					setInstrument(R,1,getInstrument(instrumentName));
+				}
+				if(line.contains("V_instrument")) {
+					String instrumentName = line.replaceAll(".*[: ]","");
+					setInstrument(V,2,getInstrument(instrumentName));
 				}
 				continue;
 			}
@@ -210,206 +210,217 @@ public class midi_generate
 		t.add(me);
 	}
 	public static void setTrackEnd(Track t) throws Exception {
-
 		long end = maxEnd+20L;
 		MetaMessage mt = new MetaMessage();
 		byte[] bet = {}; // empty array
 		mt.setMessage(0x2F,bet,0);
 		MidiEvent me = new MidiEvent(mt, end);
 		t.add(me);
-
 	}
-	
-public static final int PIANO__ACOUSTIC_GRAND_PIANO = 0;
-public static final int PIANO__BRIGHT_ACOUSTIC_PIANO = 1;
-public static final int PIANO__ELECTRIC_GRAND_PIANO = 2;
-public static final int PIANO__HONKY_TONK_PIANO = 3;
-public static final int PIANO__ELECTRIC_PIANO_1 = 4;
-public static final int PIANO__ELECTRIC_PIANO_2 = 5;
-public static final int PIANO__HARPSICHORD = 6;
-public static final int PIANO__CLAVI = 7;
-public static final int CHROMATIC_PERCUSSION__CELESTA = 8;
-public static final int CHROMATIC_PERCUSSION__GLOCKENSPIEL = 9;
-public static final int CHROMATIC_PERCUSSION__MUSIC_BOX = 10;
-public static final int CHROMATIC_PERCUSSION__VIBRAPHONE = 11;
-public static final int CHROMATIC_PERCUSSION__MARIMBA = 12;
-public static final int CHROMATIC_PERCUSSION__XYLOPHONE = 13;
-public static final int CHROMATIC_PERCUSSION__TUBULAR_BELLS = 14;
-public static final int CHROMATIC_PERCUSSION__DULCIMER = 15;
-public static final int ORGAN__DRAWBAR_ORGAN = 16;
-public static final int ORGAN__PERCUSSIVE_ORGAN = 17;
-public static final int ORGAN__ROCK_ORGAN = 18;
-public static final int ORGAN__CHURCH_ORGAN = 19;
-public static final int ORGAN__REED_ORGAN = 20;
-public static final int ORGAN__ACCORDION = 21;
-public static final int ORGAN__HARMONICA = 22;
-public static final int ORGAN__TANGO_ACCORDION = 23;
-public static final int GUITAR__ACOUSTIC_GUITAR_NYLON = 24;
-public static final int GUITAR__ACOUSTIC_GUITAR_STEEL = 25;
-public static final int GUITAR__ELECTRIC_GUITAR_JAZZ = 26;
-public static final int GUITAR__ELECTRIC_GUITAR_CLEAN = 27;
-public static final int GUITAR__ELECTRIC_GUITAR_MUTED = 28;
-public static final int GUITAR__OVERDRIVEN_GUITAR = 29;
-public static final int GUITAR__DISTORTION_GUITAR = 30;
-public static final int GUITAR__GUITAR_HARMONICS = 31;
-public static final int BASS__ACOUSTIC_BASS = 32;
-public static final int BASS__ELECTRIC_BASS_FINGER = 33;
-public static final int BASS__ELECTRIC_BASS_PICK = 34;
-public static final int BASS__FRETLESS_BASS = 35;
-public static final int BASS__SLAP_BASS_1 = 36;
-public static final int BASS__SLAP_BASS_2 = 37;
-public static final int BASS__SYNTH_BASS_1 = 38;
-public static final int BASS__SYNTH_BASS_2 = 39;
-public static final int STRINGS__VIOLIN = 40;
-public static final int STRINGS__VIOLA = 41;
-public static final int STRINGS__CELLO = 42;
-public static final int STRINGS__CONTRABASS = 43;
-public static final int STRINGS__TREMOLO_STRINGS = 44;
-public static final int STRINGS__PIZZICATO_STRINGS = 45;
-public static final int STRINGS__ORCHESTRAL_HARP = 46;
-public static final int STRINGS__TIMPANI = 47;
-public static final int ENSEMBLE__STRING_ENSEMBLE_1 = 48;
-public static final int ENSEMBLE__STRING_ENSEMBLE_2 = 49;
-public static final int ENSEMBLE__SYNTH_STRINGS_1 = 50;
-public static final int ENSEMBLE__SYNTH_STRINGS_2 = 51;
-public static final int ENSEMBLE__CHOIR_AAHS = 52;
-public static final int ENSEMBLE__VOICE_OOHS = 53;
-public static final int ENSEMBLE__SYNTH_VOICE = 54;
-public static final int ENSEMBLE__ORCHESTRA_HIT = 55;
-public static final int BRASS__TRUMPET = 56;
-public static final int BRASS__TROMBONE = 57;
-public static final int BRASS__TUBA = 58;
-public static final int BRASS__MUTED_TRUMPET = 59;
-public static final int BRASS__FRENCH_HORN = 60;
-public static final int BRASS__BRASS_SECTION = 61;
-public static final int BRASS__SYNTH_BRASS_1 = 62;
-public static final int BRASS__SYNTH_BRASS_2 = 63;
-public static final int REED__SOPRANO_SAX = 64;
-public static final int REED__ALTO_SAX = 65;
-public static final int REED__TENOR_SAX = 66;
-public static final int REED__BARITONE_SAX = 67;
-public static final int REED__OBOE = 68;
-public static final int REED__ENGLISH_HORN = 69;
-public static final int REED__BASSOON = 70;
-public static final int REED__CLARINET = 71;
-public static final int PIPE__PICCOLO = 72;
-public static final int PIPE__FLUTE = 73;
-public static final int PIPE__RECORDER = 74;
-public static final int PIPE__PAN_FLUTE = 75;
-public static final int PIPE__BLOWN_BOTTLE = 76;
-public static final int PIPE__SHAKUHACHI = 77;
-public static final int PIPE__WHISTLE = 78;
-public static final int PIPE__OCARINA = 79;
-public static final int SYNTH_LEAD__LEAD_1_SQUARE = 80;
-public static final int SYNTH_LEAD__LEAD_2_SAWTOOTH = 81;
-public static final int SYNTH_LEAD__LEAD_3_CALLIOPE = 82;
-public static final int SYNTH_LEAD__LEAD_4_CHIFF = 83;
-public static final int SYNTH_LEAD__LEAD_5_CHARANG = 84;
-public static final int SYNTH_LEAD__LEAD_6_VOICE = 85;
-public static final int SYNTH_LEAD__LEAD_7_FIFTHS = 86;
-public static final int SYNTH_LEAD__LEAD_8_BASS_WITH_LEAD = 87;
-public static final int SYNTH_PAD__PAD_1_NEW_AGE = 88;
-public static final int SYNTH_PAD__PAD_2_WARM = 89;
-public static final int SYNTH_PAD__PAD_3_POLYSYNTH = 90;
-public static final int SYNTH_PAD__PAD_4_CHOIR = 91;
-public static final int SYNTH_PAD__PAD_5_BOWED = 92;
-public static final int SYNTH_PAD__PAD_6_METALLIC = 93;
-public static final int SYNTH_PAD__PAD_7_HALO = 94;
-public static final int SYNTH_PAD__PAD_8_SWEEP = 95;
-public static final int SYNTH_EFFECTS__FX_1_RAIN = 96;
-public static final int SYNTH_EFFECTS__FX_2_SOUNDTRACK = 97;
-public static final int SYNTH_EFFECTS__FX_3_CRYSTAL = 98;
-public static final int SYNTH_EFFECTS__FX_4_ATMOSPHERE = 99;
-public static final int SYNTH_EFFECTS__FX_5_BRIGHTNESS = 100;
-public static final int SYNTH_EFFECTS__FX_6_GOBLINS = 101;
-public static final int SYNTH_EFFECTS__FX_7_ECHOES = 102;
-public static final int SYNTH_EFFECTS__FX_8_SCI_FI = 103;
-public static final int ETHNIC__SITAR = 104;
-public static final int ETHNIC__BANJO = 105;
-public static final int ETHNIC__SHAMISEN = 106;
-public static final int ETHNIC__KOTO = 107;
-public static final int ETHNIC__KALIMBA = 108;
-public static final int ETHNIC__BAG_PIPE = 109;
-public static final int ETHNIC__FIDDLE = 110;
-public static final int ETHNIC__SHANAI = 111;
-public static final int PERCUSSIVE__TINKLE_BELL = 112;
-public static final int PERCUSSIVE__AGOGO = 113;
-public static final int PERCUSSIVE__STEEL_DRUMS = 114;
-public static final int PERCUSSIVE__WOODBLOCK = 115;
-public static final int PERCUSSIVE__TAIKO_DRUM = 116;
-public static final int PERCUSSIVE__MELODIC_TOM = 117;
-public static final int PERCUSSIVE__SYNTH_DRUM = 118;
-public static final int PERCUSSIVE__REVERSE_CYMBAL = 119;
-public static final int SOUND_EFFECTS__GUITAR_FRET_NOISE = 120;
-public static final int SOUND_EFFECTS__BREATH_NOISE = 121;
-public static final int SOUND_EFFECTS__SEASHORE = 122;
-public static final int SOUND_EFFECTS__BIRD_TWEET = 123;
-public static final int SOUND_EFFECTS__TELEPHONE_RING = 124;
-public static final int SOUND_EFFECTS__HELICOPTER = 125;
-public static final int SOUND_EFFECTS__APPLAUSE = 126;
-public static final int SOUND_EFFECTS__GUNSHOT = 127;
+	public static int getInstrument(String name) {
+		return Instrument.valueOf(name).getValue();
+	}
+	public static int getSoundEffect(String name) {
+		return SoundEffect.valueOf(name).getValue();
+	}
+	enum Instrument {
+		PIANO__ACOUSTIC_GRAND_PIANO(0),
+		PIANO__BRIGHT_ACOUSTIC_PIANO(1),
+		PIANO__ELECTRIC_GRAND_PIANO(2),
+		PIANO__HONKY_TONK_PIANO(3),
+		PIANO__ELECTRIC_PIANO_1(4),
+		PIANO__ELECTRIC_PIANO_2(5),
+		PIANO__HARPSICHORD(6),
+		PIANO__CLAVI(7),
+		CHROMATIC_PERCUSSION__CELESTA(8),
+		CHROMATIC_PERCUSSION__GLOCKENSPIEL(9),
+		CHROMATIC_PERCUSSION__MUSIC_BOX(10),
+		CHROMATIC_PERCUSSION__VIBRAPHONE(11),
+		CHROMATIC_PERCUSSION__MARIMBA(12),
+		CHROMATIC_PERCUSSION__XYLOPHONE(13),
+		CHROMATIC_PERCUSSION__TUBULAR_BELLS(14),
+		CHROMATIC_PERCUSSION__DULCIMER(15),
+		ORGAN__DRAWBAR_ORGAN(16),
+		ORGAN__PERCUSSIVE_ORGAN(17),
+		ORGAN__ROCK_ORGAN(18),
+		ORGAN__CHURCH_ORGAN(19),
+		ORGAN__REED_ORGAN(20),
+		ORGAN__ACCORDION(21),
+		ORGAN__HARMONICA(22),
+		ORGAN__TANGO_ACCORDION(23),
+		GUITAR__ACOUSTIC_GUITAR_NYLON(24),
+		GUITAR__ACOUSTIC_GUITAR_STEEL(25),
+		GUITAR__ELECTRIC_GUITAR_JAZZ(26),
+		GUITAR__ELECTRIC_GUITAR_CLEAN(27),
+		GUITAR__ELECTRIC_GUITAR_MUTED(28),
+		GUITAR__OVERDRIVEN_GUITAR(29),
+		GUITAR__DISTORTION_GUITAR(30),
+		GUITAR__GUITAR_HARMONICS(31),
+		BASS__ACOUSTIC_BASS(32),
+		BASS__ELECTRIC_BASS_FINGER(33),
+		BASS__ELECTRIC_BASS_PICK(34),
+		BASS__FRETLESS_BASS(35),
+		BASS__SLAP_BASS_1(36),
+		BASS__SLAP_BASS_2(37),
+		BASS__SYNTH_BASS_1(38),
+		BASS__SYNTH_BASS_2(39),
+		STRINGS__VIOLIN(40),
+		STRINGS__VIOLA(41),
+		STRINGS__CELLO(42),
+		STRINGS__CONTRABASS(43),
+		STRINGS__TREMOLO_STRINGS(44),
+		STRINGS__PIZZICATO_STRINGS(45),
+		STRINGS__ORCHESTRAL_HARP(46),
+		STRINGS__TIMPANI(47),
+		ENSEMBLE__STRING_ENSEMBLE_1(48),
+		ENSEMBLE__STRING_ENSEMBLE_2(49),
+		ENSEMBLE__SYNTH_STRINGS_1(50),
+		ENSEMBLE__SYNTH_STRINGS_2(51),
+		ENSEMBLE__CHOIR_AAHS(52),
+		ENSEMBLE__VOICE_OOHS(53),
+		ENSEMBLE__SYNTH_VOICE(54),
+		ENSEMBLE__ORCHESTRA_HIT(55),
+		BRASS__TRUMPET(56),
+		BRASS__TROMBONE(57),
+		BRASS__TUBA(58),
+		BRASS__MUTED_TRUMPET(59),
+		BRASS__FRENCH_HORN(60),
+		BRASS__BRASS_SECTION(61),
+		BRASS__SYNTH_BRASS_1(62),
+		BRASS__SYNTH_BRASS_2(63),
+		REED__SOPRANO_SAX(64),
+		REED__ALTO_SAX(65),
+		REED__TENOR_SAX(66),
+		REED__BARITONE_SAX(67),
+		REED__OBOE(68),
+		REED__ENGLISH_HORN(69),
+		REED__BASSOON(70),
+		REED__CLARINET(71),
+		PIPE__PICCOLO(72),
+		PIPE__FLUTE(73),
+		PIPE__RECORDER(74),
+		PIPE__PAN_FLUTE(75),
+		PIPE__BLOWN_BOTTLE(76),
+		PIPE__SHAKUHACHI(77),
+		PIPE__WHISTLE(78),
+		PIPE__OCARINA(79),
+		SYNTH_LEAD__LEAD_1_SQUARE(80),
+		SYNTH_LEAD__LEAD_2_SAWTOOTH(81),
+		SYNTH_LEAD__LEAD_3_CALLIOPE(82),
+		SYNTH_LEAD__LEAD_4_CHIFF(83),
+		SYNTH_LEAD__LEAD_5_CHARANG(84),
+		SYNTH_LEAD__LEAD_6_VOICE(85),
+		SYNTH_LEAD__LEAD_7_FIFTHS(86),
+		SYNTH_LEAD__LEAD_8_BASS_WITH_LEAD(87),
+		SYNTH_PAD__PAD_1_NEW_AGE(88),
+		SYNTH_PAD__PAD_2_WARM(89),
+		SYNTH_PAD__PAD_3_POLYSYNTH(90),
+		SYNTH_PAD__PAD_4_CHOIR(91),
+		SYNTH_PAD__PAD_5_BOWED(92),
+		SYNTH_PAD__PAD_6_METALLIC(93),
+		SYNTH_PAD__PAD_7_HALO(94),
+		SYNTH_PAD__PAD_8_SWEEP(95),
+		SYNTH_EFFECTS__FX_1_RAIN(96),
+		SYNTH_EFFECTS__FX_2_SOUNDTRACK(97),
+		SYNTH_EFFECTS__FX_3_CRYSTAL(98),
+		SYNTH_EFFECTS__FX_4_ATMOSPHERE(99),
+		SYNTH_EFFECTS__FX_5_BRIGHTNESS(100),
+		SYNTH_EFFECTS__FX_6_GOBLINS(101),
+		SYNTH_EFFECTS__FX_7_ECHOES(102),
+		SYNTH_EFFECTS__FX_8_SCI_FI(103),
+		ETHNIC__SITAR(104),
+		ETHNIC__BANJO(105),
+		ETHNIC__SHAMISEN(106),
+		ETHNIC__KOTO(107),
+		ETHNIC__KALIMBA(108),
+		ETHNIC__BAG_PIPE(109),
+		ETHNIC__FIDDLE(110),
+		ETHNIC__SHANAI(111),
+		PERCUSSIVE__TINKLE_BELL(112),
+		PERCUSSIVE__AGOGO(113),
+		PERCUSSIVE__STEEL_DRUMS(114),
+		PERCUSSIVE__WOODBLOCK(115),
+		PERCUSSIVE__TAIKO_DRUM(116),
+		PERCUSSIVE__MELODIC_TOM(117),
+		PERCUSSIVE__SYNTH_DRUM(118),
+		PERCUSSIVE__REVERSE_CYMBAL(119),
+		SOUND_EFFECTS__GUITAR_FRET_NOISE(120),
+		SOUND_EFFECTS__BREATH_NOISE(121),
+		SOUND_EFFECTS__SEASHORE(122),
+		SOUND_EFFECTS__BIRD_TWEET(123),
+		SOUND_EFFECTS__TELEPHONE_RING(124),
+		SOUND_EFFECTS__HELICOPTER(125),
+		SOUND_EFFECTS__APPLAUSE(126),
+		SOUND_EFFECTS__GUNSHOT(127);
+		private int value;
+		private Instrument(int value) { this.value = value; }
+		public int getValue() { return value; }
+	}
 
-
-public static final int PERCUSSION__HIGH_Q = 27;
-public static final int PERCUSSION__SLAP = 28;
-public static final int PERCUSSION__STRATCH_PUSH = 29;
-public static final int PERCUSSION__STRATCH_PULL = 30;
-public static final int PERCUSSION__STICKS = 31;
-public static final int PERCUSSION__SQUARE_CLICK = 32;
-public static final int PERCUSSION__METRONOME_CLICK = 33;
-public static final int PERCUSSION__METRONOME_BELL = 34;
-public static final int PERCUSSION__ACOUSTIC_BASS_DRUM = 35;
-public static final int PERCUSSION__ELECTRIC_BASS_DRUM = 36;
-public static final int PERCUSSION__SIDE_STICK = 37;
-public static final int PERCUSSION__ACOUSTIC_SNARE = 38;
-public static final int PERCUSSION__HAND_CLAP = 39;
-public static final int PERCUSSION__ELECTRIC_SNARE = 40;
-public static final int PERCUSSION__LOW_FLOOR_TOM = 41;
-public static final int PERCUSSION__CLOSED_HI_HAT = 42;
-public static final int PERCUSSION__HIGH_FLOOR_TOM = 43;
-public static final int PERCUSSION__PEDAL_HI_HAT = 44;
-public static final int PERCUSSION__LOW_TOM = 45;
-public static final int PERCUSSION__OPEN_HI_HAT = 46;
-public static final int PERCUSSION__LOW_MID_TOM = 47;
-public static final int PERCUSSION__HI_MID_TOM = 48;
-public static final int PERCUSSION__CRASH_CYMBAL_1 = 49;
-public static final int PERCUSSION__HIGH_TOM = 50;
-public static final int PERCUSSION__RIDE_CYMBAL_1 = 51;
-public static final int PERCUSSION__CHINESE_CYMBAL = 52;
-public static final int PERCUSSION__RIDE_BELL = 53;
-public static final int PERCUSSION__TAMBOURINE = 54;
-public static final int PERCUSSION__SPLASH_CYMBAL = 55;
-public static final int PERCUSSION__COWBELL = 56;
-public static final int PERCUSSION__CRASH_CYMBAL_2 = 57;
-public static final int PERCUSSION__VIBRA_SLAP = 58;
-public static final int PERCUSSION__RIDE_CYMBAL_2 = 59;
-public static final int PERCUSSION__HIGH_BONGO = 60;
-public static final int PERCUSSION__LOW_BONGO = 61;
-public static final int PERCUSSION__MUTE_HIGH_CONGA = 62;
-public static final int PERCUSSION__OPEN_HIGH_CONGA = 63;
-public static final int PERCUSSION__LOW_CONGA = 64;
-public static final int PERCUSSION__HIGH_TIMBALE = 65;
-public static final int PERCUSSION__LOW_TIMBALE = 66;
-public static final int PERCUSSION__HIGH_AGOGO = 67;
-public static final int PERCUSSION__LOW_AGOGO = 68;
-public static final int PERCUSSION__CABASA = 69;
-public static final int PERCUSSION__MARACAS = 70;
-public static final int PERCUSSION__SHORT_WHISTLE = 71;
-public static final int PERCUSSION__LONG_WHISTLE = 72;
-public static final int PERCUSSION__SHORT_GUIRO = 73;
-public static final int PERCUSSION__LONG_GUIRO = 74;
-public static final int PERCUSSION__CLAVES = 75;
-public static final int PERCUSSION__HIGH_WOODBLOCK = 76;
-public static final int PERCUSSION__LOW_WOODBLOCK = 77;
-public static final int PERCUSSION__MUTE_CUICA = 78;
-public static final int PERCUSSION__OPEN_CUICA = 79;
-public static final int PERCUSSION__MUTE_TRIANGLE = 80;
-public static final int PERCUSSION__OPEN_TRIANGLE = 81;
-public static final int PERCUSSION__SHAKER = 82;
-public static final int PERCUSSION__JINGLE_BELL = 83;
-public static final int PERCUSSION__BELLTREE = 84;
-public static final int PERCUSSION__CASTANETS = 85;
-public static final int PERCUSSION__MUTE_SURDO = 86;
-public static final int PERCUSSION__OPEN_SURDO = 87;
-
-} //midifile
+	enum SoundEffect {
+		PERCUSSION__HIGH_Q(27),
+		PERCUSSION__SLAP(28),
+		PERCUSSION__STRATCH_PUSH(29),
+		PERCUSSION__STRATCH_PULL(30),
+		PERCUSSION__STICKS(31),
+		PERCUSSION__SQUARE_CLICK(32),
+		PERCUSSION__METRONOME_CLICK(33),
+		PERCUSSION__METRONOME_BELL(34),
+		PERCUSSION__ACOUSTIC_BASS_DRUM(35),
+		PERCUSSION__ELECTRIC_BASS_DRUM(36),
+		PERCUSSION__SIDE_STICK(37),
+		PERCUSSION__ACOUSTIC_SNARE(38),
+		PERCUSSION__HAND_CLAP(39),
+		PERCUSSION__ELECTRIC_SNARE(40),
+		PERCUSSION__LOW_FLOOR_TOM(41),
+		PERCUSSION__CLOSED_HI_HAT(42),
+		PERCUSSION__HIGH_FLOOR_TOM(43),
+		PERCUSSION__PEDAL_HI_HAT(44),
+		PERCUSSION__LOW_TOM(45),
+		PERCUSSION__OPEN_HI_HAT(46),
+		PERCUSSION__LOW_MID_TOM(47),
+		PERCUSSION__HI_MID_TOM(48),
+		PERCUSSION__CRASH_CYMBAL_1(49),
+		PERCUSSION__HIGH_TOM(50),
+		PERCUSSION__RIDE_CYMBAL_1(51),
+		PERCUSSION__CHINESE_CYMBAL(52),
+		PERCUSSION__RIDE_BELL(53),
+		PERCUSSION__TAMBOURINE(54),
+		PERCUSSION__SPLASH_CYMBAL(55),
+		PERCUSSION__COWBELL(56),
+		PERCUSSION__CRASH_CYMBAL_2(57),
+		PERCUSSION__VIBRA_SLAP(58),
+		PERCUSSION__RIDE_CYMBAL_2(59),
+		PERCUSSION__HIGH_BONGO(60),
+		PERCUSSION__LOW_BONGO(61),
+		PERCUSSION__MUTE_HIGH_CONGA(62),
+		PERCUSSION__OPEN_HIGH_CONGA(63),
+		PERCUSSION__LOW_CONGA(64),
+		PERCUSSION__HIGH_TIMBALE(65),
+		PERCUSSION__LOW_TIMBALE(66),
+		PERCUSSION__HIGH_AGOGO(67),
+		PERCUSSION__LOW_AGOGO(68),
+		PERCUSSION__CABASA(69),
+		PERCUSSION__MARACAS(70),
+		PERCUSSION__SHORT_WHISTLE(71),
+		PERCUSSION__LONG_WHISTLE(72),
+		PERCUSSION__SHORT_GUIRO(73),
+		PERCUSSION__LONG_GUIRO(74),
+		PERCUSSION__CLAVES(75),
+		PERCUSSION__HIGH_WOODBLOCK(76),
+		PERCUSSION__LOW_WOODBLOCK(77),
+		PERCUSSION__MUTE_CUICA(78),
+		PERCUSSION__OPEN_CUICA(79),
+		PERCUSSION__MUTE_TRIANGLE(80),
+		PERCUSSION__OPEN_TRIANGLE(81),
+		PERCUSSION__SHAKER(82),
+		PERCUSSION__JINGLE_BELL(83),
+		PERCUSSION__BELLTREE(84),
+		PERCUSSION__CASTANETS(85),
+		PERCUSSION__MUTE_SURDO(86),
+		PERCUSSION__OPEN_SURDO(87);
+		private int value;
+		private SoundEffect(int value) { this.value = value; }
+		public int getValue() { return value; }
+	}
+}
